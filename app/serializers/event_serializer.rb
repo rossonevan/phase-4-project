e556f9c4f298025ea87c1be9d48ee19d64781a9e
xauvk <xauvkubalexanderyang@live.com>
2022-09-14 13:10:10 -0400

    can remove tickets kinda

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,4 +1,5 @@
 class EventSerializer < ActiveModel::Serializer
   attributes :id, :name, :location, :date, :time, :price, :image
+  has_many :tickets
   
 end
--- client/src/Components/UserPage.js
+++ client/src/Components/UserPage.js
@@ -4,6 +4,7 @@ import { useHistory, Link} from "react-router-dom";
 function UserPage({updateUser, currentUser}) {
     const [loading, setLoading] = useState(true)
     const [errors, setErrors] = useState(false)
+    const [tickets, setTickets] = useState([])
 
     const history = useHistory()
 
@@ -14,22 +15,28 @@ function UserPage({updateUser, currentUser}) {
             .then(updateUser())
     }
 
-    const removeTicket = () => {
-        fetch(`/tickets/`, {method: 'DELETE'})
+    const removeTicket = (id) => {
+        fetch(`/tickets/${id}`, {method: 'DELETE'})
+        .then(getTickets())
     }
 
-    useEffect(() => {
+    const getTickets = () => {
         fetch(`/me`)
         .then(res => {
             if(res.ok){
                 res.json().then(user => {
                     updateUser(user)
                     setLoading(false)
+                    setTickets(currentUser.tickets)
                 })
             } else {
                 res.json().then(data => setErrors(data.error))
             }
         })
+    }
+
+    useEffect(() => {
+        getTickets()
     }, [])
 
     if(loading) return <h1>Loading...</h1>
@@ -39,15 +46,17 @@ function UserPage({updateUser, currentUser}) {
         <div>
             <h3>My Tickets</h3>
             <ul>
-                {currentUser.events.map(event => (
-                    <div>
-                        <h2>{event.name}</h2>
-                        <p>Price: {event.price}</p>
-                        <button onClick={removeTicket}>Remove</button>
-                        <img src={event.image} alt={event.name} />
-                        
-                    </div>
-                ))}
+                {tickets.map(ticket => {
+                    return (
+                        <div>
+                            {/* <h2>{.name}</h2>
+                            <p>Price: {event.price}</p> */}
+                            <button onClick={() => {removeTicket(ticket.id)}}>Remove</button>
+                            {/* <img src={event.image} alt={event} /> */}
+                        </div>
+                    )
+                })
+            }
             </ul>
             <Link to='/editUser'>Edit Account</Link>
             <button onClick={deleteUser}>Delete Account</button>
